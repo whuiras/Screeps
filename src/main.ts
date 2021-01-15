b6ec2362fb99bcdd718d70f06183f2aa34ef9429
Will Huiras <whuiras@pdx.edu>
2021-01-15 11:07:37 -0800

    refactoring and reformatting

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,40 @@
 import { ErrorMapper } from "utils/ErrorMapper";
-import {BootstrapStrategy} from "./mainStrategy/BootstrapStrategy";
-import {RunStrategy} from "./mainStrategy/RunStrategy";
+import { BootstrapStrategy } from "./mainStrategy/BootstrapStrategy";
+import { RunStrategy } from "./mainStrategy/RunStrategy";
 import { AbstractStrategy } from "./mainStrategy/AbstractStrategy";
 import { MemoryHandler } from "./memory/MemoryHandler";
 import { Logger } from "./Logger";
+import * as Profiler from "../libraries/Profiler";
 
+// global.profiler = Profiler.init();
+const memoryHandler: MemoryHandler = new MemoryHandler();
+const logger: Logger = new Logger(3);
 
-const memoryHandler : MemoryHandler = new MemoryHandler()
 
 export const loop = ErrorMapper.wrapLoop(() => {
 
   if (Game.time % 10 === 0) {
-    console.log(`Current game tick is ${Game.time}`)
+    console.log(`Current game tick is ${Game.time}`);
   }
 
-  if (Memory.initialized !== true) {
-    const logger = new Logger(3)
-    Memory.initialized = true
-  }
-
-  memoryHandler.updateMemory()
+  memoryHandler.updateMemory();
 
   // Assign main algorithm
-  let mainStrategy:AbstractStrategy = new RunStrategy()
+  let mainStrategy: AbstractStrategy = new RunStrategy();
   if (Object.keys(Game.rooms).length === 1) {
-    const room = Object.values(Game.rooms).find((r) => r.controller && r.controller.my)
+    const room = Object.values(Game.rooms).find((r) => r.controller && r.controller.my);
 
     if (room !== undefined && room.controller !== undefined && room.controller.level <= 9) {
-      console.log("Bootstrapping")
-      mainStrategy = new BootstrapStrategy()
+      console.log("Bootstrapping");
+      mainStrategy = new BootstrapStrategy();
     }
 
   } else {
-    mainStrategy = new RunStrategy()
+    mainStrategy = new RunStrategy();
   }
 
   if (mainStrategy) {
-    mainStrategy.execute()
+    mainStrategy.execute();
   }
 
 
