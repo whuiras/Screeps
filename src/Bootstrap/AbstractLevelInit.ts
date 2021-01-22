dc8a8d2a9dc45b88297f04e8b1e1bdd42d2a2fa2
Will Huiras <whuiras@pdx.edu>
2021-01-21 20:06:48 -0800

    added support for building level plans retroactively

--- src/Bootstrap/AbstractLevelInit.ts
+++ src/Bootstrap/AbstractLevelInit.ts
@@ -1,7 +1,13 @@
 import { AbstractPhase } from "./AbstractPhase";
-import { RCL1Structures } from "../RCLConstants";
-import { isNumber } from "util";
 import { Logger } from "../Logger";
+import { Stage1Init } from "./Stage1/Stage1Init";
+import { Stage2Init } from "./Stage2/Stage2Init";
+import { Stage3Init } from "./Stage3/Stage3Init";
+import { Stage4Init } from "./Stage4/Stage4Init";
+import { Stage5Init } from "./Stage5/Stage5Init";
+import { Stage6Init } from "./Stage6/Stage6Init";
+import { Stage7Init } from "./Stage7/Stage7Init";
+import { Stage8Init } from "./Stage8/Stage8Init";
 
 export abstract class AbstractLevelInit extends AbstractPhase {
 
@@ -10,11 +16,60 @@ export abstract class AbstractLevelInit extends AbstractPhase {
     this.roomMem.levelInit = true
   }
 
+  protected buildPreviousLevels(upTo: number): void {
+    for (let i = 0; i <= upTo; i++) {
+      if (!this.roomMem.roomPlan.levelBuilt[i]) {
+        // build the level
+        // It would be great if I could use static methods here, though they are dependant on roomMemory. :(
+        let initInstance: AbstractLevelInit | undefined
+        switch (i) {
+          case 1:
+            initInstance = new Stage1Init(this.roomMem.id as Id<Room>)
+            break;
+          case 2:
+            initInstance = new Stage2Init(this.roomMem.id as Id<Room>)
+            break;
+          case 3:
+            initInstance = new Stage3Init(this.roomMem.id as Id<Room>)
+            break;
+          case 4:
+            initInstance = new Stage4Init(this.roomMem.id as Id<Room>)
+            break;
+          case 5:
+            initInstance = new Stage5Init(this.roomMem.id as Id<Room>)
+            break;
+          case 6:
+            initInstance = new Stage6Init(this.roomMem.id as Id<Room>)
+            break;
+          case 7:
+            initInstance = new Stage7Init(this.roomMem.id as Id<Room>)
+            break;
+          case 8:
+            initInstance = new Stage8Init(this.roomMem.id as Id<Room>)
+            break;
+          default:
+            initInstance = undefined
+        }
+        if (initInstance !== undefined) {
+          initInstance.fillBuildQueue()
+        } else {
+          Logger.logError("buildPreviousLevels() received invalid level literal")
+        }
+      }
+    }
+  }
+
+  /**
+   * Calls buildPreviousLevels from the subclass, passing on upTo parameter
+   * @protected
+   */
+  protected abstract checkPreviousBuilds(): void
+
   protected abstract runInit(): void
 
   protected abstract fillBuildQueue(): void
 
-  protected fillStructurePlan(toFill: number, type: string):void {
+  protected fillStructurePlan(toFill: number, type: BuildableStructureConstant):void {
     let structurePlanMem
     switch (type) {
       case STRUCTURE_ROAD:
@@ -72,11 +127,28 @@ export abstract class AbstractLevelInit extends AbstractPhase {
       case STRUCTURE_POWER_SPAWN:
         structurePlanMem = this.roomMem.roomPlan.powerSpawn;
         break;
+
+      default:
+        structurePlanMem = undefined
+        break;
     }
     if (structurePlanMem !== undefined) {
       for (let i = 0; i < toFill; i++) {
         const coord = structurePlanMem.shift()
         if (coord !== undefined) {
+          const room: Room = Game.rooms[this.roomMem.id]
+          const result = room.createConstructionSite(coord[0], coord[1], type)
+
+          // Check to see if we can build at the coordinates
+          // If not, grab another coordinate off the build plan
+          if (result !== OK) {
+            Logger.logWarn(
+              "Could not create construction site at [" +
+              coord[0].toString() + ',' + coord[1].toString() + ']')
+            Logger.logWarn("Returned error code: " + result.toString())
+            i--;
+            continue;
+          }
           this.roomMem.roomPlan.buildQueue.push([coord[0], coord[1], type])
         } else {
           Logger.logError("Build plan coord is undefined")
