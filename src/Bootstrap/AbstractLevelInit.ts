6b0eb6306a6e44c9fa90365a3abd0edb0c5e3c28
Will Huiras <whuiras@pdx.edu>
2021-01-28 22:08:34 -0800

    refactoring + addes construction sites to mem

--- src/Bootstrap/AbstractLevelInit.ts
+++ src/Bootstrap/AbstractLevelInit.ts
@@ -12,78 +12,87 @@ export abstract class AbstractLevelInit extends AbstractPhase {
 
   protected abstract fillBuildQueue(): void
 
+  /**
+   *
+   *
+   * Notes: getting references to room memory should be done in the memory handler, not here.
+   *
+   * @param toFill The number of building to add up to
+   * @param type The type of building to add to the queue
+   * @protected
+   */
   protected fillStructurePlan(toFill: number, type: BuildableStructureConstant):void {
     let structurePlanMem
-    let extantCount = 0;
+    let structMem = [];
     switch (type) {
       case STRUCTURE_ROAD:
         structurePlanMem = this.roomMem.roomPlan.roads;
-        extantCount = this.roomMem.structures.roads.length;
+        structMem = this.roomMem.structures.roads;
         break;
 
       case STRUCTURE_CONTAINER:
         structurePlanMem = this.roomMem.roomPlan.containers;
-        extantCount = this.roomMem.structures.containers.length;
+        structMem = this.roomMem.structures.containers;
         break;
 
       case STRUCTURE_SPAWN:
         structurePlanMem = this.roomMem.roomPlan.spawns;
-        extantCount = this.roomMem.structures.spawns.length;
+        structMem = this.roomMem.structures.spawns;
         break;
 
       case STRUCTURE_EXTENSION:
         structurePlanMem = this.roomMem.roomPlan.extensions;
-        extantCount = this.roomMem.structures.extensions.length;
+        structMem = this.roomMem.structures.extensions;
         break;
 
       case STRUCTURE_RAMPART:
         structurePlanMem = this.roomMem.roomPlan.ramparts;
-        extantCount = this.roomMem.structures.ramparts.length;
+        structMem = this.roomMem.structures.ramparts;
         break;
 
       case STRUCTURE_WALL:
         structurePlanMem = this.roomMem.roomPlan.walls;
-        extantCount = this.roomMem.structures.walls.length;
+        structMem = this.roomMem.structures.walls;
         break;
 
       case STRUCTURE_TOWER:
         structurePlanMem = this.roomMem.roomPlan.towers;
-        extantCount = this.roomMem.structures.towers.length;
+        structMem = this.roomMem.structures.towers;
         break;
 
       case STRUCTURE_STORAGE:
         structurePlanMem = this.roomMem.roomPlan.storage;
-        extantCount = this.roomMem.structures.storage.length;
+        structMem = this.roomMem.structures.storage;
         break;
 
       case STRUCTURE_LINK:
         structurePlanMem = this.roomMem.roomPlan.links;
-        extantCount = this.roomMem.structures.links.length;
+        structMem = this.roomMem.structures.links;
         break;
 
       case STRUCTURE_EXTRACTOR:
         structurePlanMem = this.roomMem.roomPlan.extractors;
-        extantCount = this.roomMem.structures.extractors.length;
+        structMem = this.roomMem.structures.extractors;
         break;
 
       case STRUCTURE_LAB:
         structurePlanMem = this.roomMem.roomPlan.labs;
-        extantCount = this.roomMem.structures.labs.length;
+        structMem = this.roomMem.structures.labs;
         break;
 
       case STRUCTURE_TERMINAL:
         structurePlanMem = this.roomMem.roomPlan.terminal;
-        extantCount = this.roomMem.structures.terminal.length;
+        structMem = this.roomMem.structures.terminal;
         break;
 
       case STRUCTURE_OBSERVER:
         structurePlanMem = this.roomMem.roomPlan.observer;
-        extantCount = this.roomMem.structures.observer.length;
+        structMem = this.roomMem.structures.observer;
         break;
 
       case STRUCTURE_POWER_SPAWN:
         structurePlanMem = this.roomMem.roomPlan.powerSpawn;
-        extantCount = this.roomMem.structures.powerSpawn.length;
+        structMem = this.roomMem.structures.powerSpawn;
         break;
 
       default:
@@ -92,7 +101,7 @@ export abstract class AbstractLevelInit extends AbstractPhase {
     }
     if (structurePlanMem !== undefined) {
 
-      for (let i = extantCount; i < toFill; i++) {
+      for (let i = structMem.length; i < toFill; i++) {
         const coord = structurePlanMem[i]
         if (coord !== undefined) {
           const room: Room = Game.rooms[this.roomMem.id]
@@ -109,6 +118,8 @@ export abstract class AbstractLevelInit extends AbstractPhase {
             continue;
           }
           this.roomMem.buildQueue.push([coord[0], coord[1], type])
+          structMem.push(coord[0], coord[1])
+
         } else {
           Logger.logError("Build plan coord is undefined")
         }
