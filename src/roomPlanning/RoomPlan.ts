9769ae55a7adb5526321717b7fcabb15881a8a7f
Will Huiras <whuiras@pdx.edu>
2021-02-01 21:46:11 -0800

    added room plan class to be passed to mem

--- src/roomPlanning/RoomPlan.ts
+++ src/roomPlanning/RoomPlan.ts
@@ -1,4 +1,20 @@
 export class RoomPlan {
-  private roads = []
-  public ex
+  public coreRoads: number[][] = [];
+  public capRoads: number[][] = [];
+  public containers: number[][] = [];
+  public spawn: number[][] = [];
+  public extensions: number[][] = [];
+  public ramparts: number[][] = [];
+  public walls: number[][] = [];
+  public towers: number[][] = [];
+  public storage: number[][] = [];
+  public coreLinks: number[][] = [];
+  public sourceLinks: number[][] = [];
+  public controllerLinks: number[][] = [];
+  public extractor: number[][] = [];
+  public labs: number[][] = [];
+  public terminal: number[][] = [];
+  public observer: number[][] = [];
+  public powerSpawn: number[][] = [];
+  public coreEdges: number[][] = [];
 }
