ff046579677f1d13ae1c8270b96b5b06942605a5
Will Huiras <whuiras@pdx.edu>
2021-02-05 20:01:22 -0800

    deleted old code

--- src/memory/MemoryHandler.ts
+++ src/memory/MemoryHandler.ts
@@ -34,67 +34,6 @@ export class MemoryHandler {
    */
   public updateRooms(): void {
 
-    if (Memory.initRoom === undefined) {
-      Memory.initRoom = {
-        buildQueue: [],
-        controller: "",
-        id: "",
-        init: false,
-        lastUpdated: 0,
-        levelInit: false,
-        maxHarvesters: 0,
-        roomPlan: {
-          coreEdges: [],
-          containers: [],
-          extensions: [],
-          roads: [],
-          coreRoads: [],
-          capRoads: [],
-          towers: [],
-          walls: [],
-          ramparts: [],
-          links: [],
-          coreLinks: [],
-          controllerLinks: [],
-          sourceLinks: [],
-          storage: [],
-          spawns: [],
-          extractors: [],
-          labs: [],
-          terminal: [],
-          observer: [],
-          powerSpawn: []
-        },
-        sources: [],
-        spawn: "",
-        structures: {
-          coreEdges: [],
-          containers: [],
-          extensions: [],
-          roads: [],
-          coreRoads: [],
-          capRoads: [],
-          towers: [],
-          walls: [],
-          ramparts: [],
-          links: [],
-          coreLinks: [],
-          controllerLinks: [],
-          sourceLinks: [],
-          storage: [],
-          spawns: [],
-          extractors: [],
-          labs: [],
-          terminal: [],
-          observer: [],
-          powerSpawn: []
-        },
-      }
-      this.initRoomMem(Memory.initRoom, Object.values(Game.rooms)[0]);
-      console.log(Memory.initRoom);
-      console.log(typeof Memory.initRoom);
-    }
-
     if (Memory.rooms === undefined) {
       Memory.rooms = [];
     }
@@ -206,52 +145,5 @@ export class MemoryHandler {
       }
     }
   }
-
-  private calcMaxRoomHarvesters(room: Room): number {
-    const sources: Source[] = room.find(FIND_SOURCES);
-    let sum = 0;
-
-    for (const source of sources) {
-      sum += this.calcMaxSourceHarvesters(room, source);
-    }
-    return sum;
-  }
-
-  private calcMaxSourceHarvesters(room: Room, source: Source): number {
-    const position = source.pos;
-    let sum = 0;
-    sum += this.symmScan(room, position.x, position.y, 0, 1);
-    sum += this.symmScan(room, position.x, position.y, 1, 1);
-    return sum;
-  }
-
-  /**
-   * Checks whether a room coordinate is in bounds
-   * @param x
-   * @param y
-   * @private
-   */
-  private isInBounds(x: number, y: number): boolean {
-    return (x > 0 && x < 49 && y > 0 && y < 49);
-  }
-
-  private symmScan(room: Room, x: number, y: number, dx: number, dy: number): number {
-    let sum = 0;
-    for (let i = 0; i < 4; i++) {
-      const candidateX = x + dx;
-      const candidateY = y + dy;
-      if (this.isInBounds(candidateX, candidateY)) {
-        if (room.getTerrain().get(candidateX, candidateY) !== TERRAIN_MASK_WALL) {
-          sum++;
-        }
-      }
-      const temp = dy;
-      dy = dx;
-      dx = temp;
-      dy = -dy;
-    }
-    return sum;
-  }
-
-
+  
 }
